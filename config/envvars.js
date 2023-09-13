0d307b7162f855d3c2054012983368d40e035d68
hftamayo <hftamayo@gmail.com>
2023-09-13 10:07:56 -0600

    fixing typo and adding export

--- config/envvars.js
+++ config/envvars.js
@@ -7,4 +7,4 @@ const masterKey = process.env.JWT_SECRET;
 const refreshKey = process.env.JWT_REFRESH;
 const mode = process.env.EXEC_MODE;
 
-export { port, datasourcce, masterKey, refreshKey, mode };
+module.exports = { port, datasource, masterKey, refreshKey, mode };
